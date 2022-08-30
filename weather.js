8259782199ee143c82cc41a74aad3aec3cd06d13
Alfredo <fredy_rios0123@outlook.com>
2022-08-29 19:52:02 -0700

    Lab 10 compleated

--- weather.js
+++ weather.js
@@ -1,15 +1,20 @@
 const axios = require(`axios`);
 
+let cache = {};
+
 let getWeather = async (request, response, next) => {
-  try {
+  let cacheExpire = 20;
+  let cacheKey = `${request.query.searchQuery}Cache`;
+  console.log(cacheKey);
+  if (cache[cacheKey] && Date.now() - cache[cacheKey].timeStamp < cacheExpire) {
     let lat = request.query.lat;
     let lon = request.query.lon;
     let urlWeather = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`
     let weatherObj = await axios.get(urlWeather);
     let selectedCity = weatherObj.data.data.map (day => new Forecast(day));
     response.send(selectedCity);
-  } catch(err) {
-    next(err)
+  // } catch(err) {
+  //   next(err)
   }
 };
 
